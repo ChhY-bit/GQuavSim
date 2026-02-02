7481dadee2574d0c5fa7c28083ba24515bc97991
ChhY-bit <1774808359@qq.com>
2026-02-02 11:13:55 +0800

    feat: 添加标准模板文件

--- Templates/StandartTemplate.m
+++ Templates/StandartTemplate.m
@@ -1,5 +1,4 @@
 % A PID Control Demo for GQUAV
-% This example demonstrates how GQuavSim works with a PID controller.
 % Each module is designed independently, allowing you to add your own custom code.
 % You can edit the code or parameters noted with "***"
 clear,clc
@@ -31,14 +30,13 @@ N = length(tspan);
 u = [0;0;0;0];
 % *** BEGIN: 用户初始化 ***
 % 此处用于定义用户的初始化代码，如轨迹生成、控制所需中间变量等
-TrajGen = GQUAV_Utils_TrajGen(tspan);   % 初始化轨迹生成器
-global last_err last_last_err last_u
-last_err = [0;0;0];
-last_last_err = [0;0;0];
-last_u = [0;0;0];       % PID中间变量
-global x y z psi %#ok<*GVMIS>
-[x,y,z] = TrajGen.SpiralTraj(1,1,1,0.1,0.5,1); % 生成螺旋轨迹
-psi = 0.5*ones(N,1);    % 偏航角
+
+% e.g.
+%TrajGen = GQUAV_Utils_TrajGen(tspan);   % 初始化轨迹生成器
+%global x y z psi %#ok<*GVMIS>
+%[x,y,z] = TrajGen.SpiralTraj(1,1,1,0.1,0.5,1); % 生成螺旋轨迹
+%psi = 0.5*ones(N,1);    % 偏航角
+
 % *** END: 用户初始化 ***
 %% 控制过程
 fprintf('开始控制，持续时间: %.2f (s)\n',T);
@@ -64,27 +62,14 @@ GQUAV_Environment_Close(SimEnv,Interface);
 %   u           -   将要被执行的控制量
 function u = CustomController(feedback,k)
 % *** 预处理部分 ***
-    global freq_ctr
-    global x y z psi
-    global last_last_err last_err last_u
-    % PID参数
-    Kp = 1.5*[1;1;1];
-    Ki = 0.2*[1;1;1];
-    Kd = 1.5*[1.35;1.35;1.5];
+% 通常用于定义和初始化参数
+
+
 % *** 核心部分 ****
-    err = [x(k);y(k);z(k)] - feedback(1:3); % 获取误差
-    du = Kp.* (err - last_err) + ...
-         Ki.* err / freq_ctr + ...
-         Kd.* (err - 2*last_err + last_last_err) * freq_ctr;
-    u_temp = last_u + du;
-    %u(1:3) = max(min(u_temp, 5), -5);    % 加速度限幅（正负5）
-    u(1:3) = u_temp;
-    u(4) = psi(k);
+% 通常为控制算法主要内容
     
 % *** 后处理部分 ***
-    last_last_err = last_err;    % 更新历史误差
-    last_err = err;
-    last_u = u_temp;
+% 通常用于迭代更新、记录保存等
 end
 %% *** 其他自定义函数 ***
 % 此处用于定义其他的函数，如观测器、滤波器等
